52adf94015742695a7aaa08743ec5abab71051eb
HrabrovSergeyM <69889536+HrabrovSergeyM@users.noreply.github.com>
2024-01-23 10:25:54 +0300

    Initial Commit

---
+++ BetRaiting/BetRaitingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetRaitingApp.swift
+//  BetRaiting
+//
+//  Created by Sergey Hrabrov on 23.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct BetRaitingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
