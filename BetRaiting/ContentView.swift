18a309278002e226f300a5996f0954c850ad4c5a
HrabrovSergeyM <69889536+HrabrovSergeyM@users.noreply.github.com>
2024-01-27 02:28:23 +0300

    main: Updated UI

--- BetRaiting/ContentView.swift
+++ BetRaiting/ContentView.swift
@@ -11,29 +11,39 @@ struct ContentView: View {
     var body: some View {
         ScrollView {
             VStack {
-                BarView(coefficients: [1.94, 2.17, 1.26])
+                Group {
+                    Text("Первая часть")
+                    BarView(coefficients: [1.94, 2.17, 1.26])
+                    Divider()
+                    BarView(coefficients: [1, 2, 3])
+                    Divider()
+                    BarView(coefficients: [0.1, 0.2, 0.3])
+                }
+                .padding(.vertical, 8)
+                
                 Divider()
-                BarView(coefficients: [1, 2, 3])
-                Divider()
-                BarView(coefficients: [0.1, 0.2, 0.3])
-                Divider()
-                MultiProgressBarView(bets: [11, 5, 6], logoImage: Image("bet_logo"))
-                Divider()
-                MultiProgressBarView(bets: [16, 11, 5], logoImage: Image("bet_logo"))
-                Divider()
-                MultiProgressBarView(bets: [35, 5, 6], logoImage: Image("bet_logo"))
-                Divider()
-                MultiProgressBarView(bets: [98, 1, 1], logoImage: Image("bet_logo"))
-                Divider()
-                MultiProgressBarView(bets: [1, 1, 1], logoImage: Image("bet_logo"))
-                Divider()
-                MultiProgressBarView(bets: [1, 0, 0], logoImage: Image("bet_logo"))
-                Divider()
-                MultiProgressBarView(bets: [1, 0, 1], logoImage: Image("bet_logo"))
-                Divider()
-                MultiProgressBarView(bets: [1, 1, 2], logoImage: Image("bet_logo"))
-                Divider()
-                MultiProgressBarView(bets: [0, 0, 0], logoImage: Image("bet_logo"))
+                
+                Group {
+                    Text("Вторая часть")
+                    MultiProgressBarView(bets: [11, 5, 6], logoImage: Image("bet_logo"))
+                    Divider()
+                    MultiProgressBarView(bets: [16, 11, 5], logoImage: Image("bet_logo"))
+                    Divider()
+                    MultiProgressBarView(bets: [35, 5, 6], logoImage: Image("bet_logo"))
+                    Divider()
+                    MultiProgressBarView(bets: [98, 1, 1], logoImage: Image("bet_logo"))
+                    Divider()
+                    MultiProgressBarView(bets: [1, 1, 1], logoImage: Image("bet_logo"))
+                    Divider()
+                    MultiProgressBarView(bets: [1, 0, 0], logoImage: Image("bet_logo"))
+                    Divider()
+                    MultiProgressBarView(bets: [1, 0, 1], logoImage: Image("bet_logo"))
+                    Divider()
+                    MultiProgressBarView(bets: [1, 1, 2], logoImage: Image("bet_logo"))
+                    Divider()
+                    MultiProgressBarView(bets: [0, 0, 0], logoImage: Image("bet_logo"))
+                }
+                .padding(.vertical, 8)
             }
         }
     }
